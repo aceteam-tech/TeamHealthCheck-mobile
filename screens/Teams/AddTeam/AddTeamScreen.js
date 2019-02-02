15cccebf89f26de80747c71ee18c3340232671fe
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-02-02 18:35:28 +0100

    Add Team screen redesign

--- screens/Teams/AddTeam/AddTeamScreen.js
+++ screens/Teams/AddTeam/AddTeamScreen.js
@@ -1,40 +1,16 @@
 import React from 'react';
-import {Text, Input, Label, Item, Form} from 'native-base'
-import {KeyboardAvoidingView, Image, View} from 'react-native'
+import {Input, Label, Item, Form, Icon} from 'native-base'
+import {KeyboardAvoidingView, TouchableOpacity} from 'react-native'
 import styled from 'styled-components/native'
-import colors from '../../../constants/Colors'
 import {addTeam} from '../../../adapters/api'
-import {getSession} from '../../../adapters/auth'
-import IconUpload from './upload-2x.png'
 import Button from '../../../components/Button/Button.component'
 import TeamLogo from '../../../components/TeamLogo/TeamLogo.component'
+import {labelStyle, inputStyle} from '../../../constants/Style'
+import Header from '../../../components/Header'
+import Page from '../../../components/Page'
 
-const Page = styled.View`
-    flex: 1;
-    backgroundColor: ${colors.primary};
-`
-
-const Header = styled.View`
-    margin-top: 40px;
-    backgroundColor: ${colors.primary};
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
-    flex: 1;
-`
-
-const HeaderText = styled.Text`
-  color: ${colors.air};
-  font-size: 20px;
-  font-weight: bold;
-`
-
-const UploadText = styled.Text`
-  color: ${colors.air};
-`
-
-const UploadButton = styled.TouchableOpacity`
-  justify-content: center;
+const LogoWrapper = styled.View`
+  flex: 1;
   align-items: center;
 `
 
@@ -44,9 +20,9 @@ const Footer = styled.View`
     margin-bottom: 30px;
 `
 
-const inputStyle = {
-    color: colors.air
-}
+const HeaderWrapper = styled.View`
+  margin-bottom: 30px;
+`
 
 export default class AddTeamScreen extends React.Component {
     static navigationOptions = {
@@ -68,45 +44,36 @@ export default class AddTeamScreen extends React.Component {
         this.props.navigation.push('Teams')
     }
 
-    async componentDidMount () {
-        const session = await getSession()
-        console.log(session);
-    }
-
     render () {
+        const {goBack} = this.props.navigation
         return (
             <Page>
-                <Header>
-                    <HeaderText>Add New Team</HeaderText>
-                </Header>
-                <View style={{flex: 6}}>
-                    <UploadButton onPress={() => {
-                    }}>
-                        <Image source={IconUpload}
-                               resizeMode='contain'
-                               style={{height: 120}}/>
-                        <UploadText>Upload team logo</UploadText>
-                    </UploadButton>
-                    <KeyboardAvoidingView behavior="padding"
-                                          keyboardVerticalOffset={20}>
-                        <Form style={{justifyContent: 'space-around', marginRight: 15, marginTop: 30}}>
-                            <Item>
-                                {
-                                    !!this.state.name &&
-                                    <TeamLogo name={this.state.name} size={50}/>
-                                }
-
-                                {/*<Label style={labelStyle}>Team name</Label>*/}
-                                <Input style={inputStyle}
-                                       autoCorrect={false}
-                                       autoCapitalize='words'
-                                       textContentType='givenName'
-                                       value={this.state.name}
-                                       onChangeText={(val) => this.onTextChange('name', val)}/>
-                            </Item>
-                        </Form>
-                    </KeyboardAvoidingView>
-                </View>
+                <HeaderWrapper>
+                    <Header title='Add Team' left={
+                        <TouchableOpacity onPress={() => goBack(null)}>
+                            <Icon name='ios-arrow-back'
+                                  type='Ionicons'
+                                  style={{color: '#FFF', fontSize: 30}}/>
+                        </TouchableOpacity>
+                    }/>
+                </HeaderWrapper>
+                <LogoWrapper>
+                    <TeamLogo name={this.state.name} size={100}/>
+                </LogoWrapper>
+                <KeyboardAvoidingView behavior="padding"
+                                      keyboardVerticalOffset={20}>
+                    <Form style={{justifyContent: 'space-around', marginRight: 15, marginTop: 30}}>
+                        <Item floatingLabel>
+                            <Label style={labelStyle}>Team name</Label>
+                            <Input style={inputStyle}
+                                   autoCorrect={false}
+                                   autoCapitalize='words'
+                                   textContentType='givenName'
+                                   value={this.state.name}
+                                   onChangeText={(val) => this.onTextChange('name', val)}/>
+                        </Item>
+                    </Form>
+                </KeyboardAvoidingView>
                 <Footer>
                     <Button version='secondary' onPress={() => this.addTeam()} text='Add'/>
                 </Footer>
