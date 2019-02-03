6a27d9dd12d712e1a413f230da4320a03c58f0ee
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-02-03 14:14:28 +0100

    Joining the team

--- navigation/TeamsNavigator.js
+++ navigation/TeamsNavigator.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import {createStackNavigator} from 'react-navigation';
-
-import TeamsScreen from '../screens/Teams/TeamsScreen';
-import AddTeamScreen from '../screens/Teams/AddTeam/AddTeamScreen';
-import MainTabNavigator from './MainTabNavigator';
+import React from 'react'
+import {createStackNavigator} from 'react-navigation'
+import TeamsScreen from '../screens/Teams/TeamsScreen'
+import AddTeamScreen from '../screens/Teams/AddTeam/AddTeamScreen'
+import JoinTeamScreen from '../screens/Teams/JoinTeamScreen'
+import MainTabNavigator from './MainTabNavigator'
 
 export default createStackNavigator({
     Teams: TeamsScreen,
     AddTeam: AddTeamScreen,
+    JoinTeam: JoinTeamScreen,
     Main: MainTabNavigator,
 }, {
     headerMode: 'none'
