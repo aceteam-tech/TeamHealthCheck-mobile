cca7820cee798cbee93cc5a8c91efb8539377fc3
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-04-03 19:56:24 +0200

    Caching error screen assets

--- App.js
+++ App.js
@@ -7,13 +7,16 @@ export default class App extends React.Component {
         // Backgrounds
         require('./components/Page/background.png'),
         require('./components/Page/background-v2.png'),
+        require('./components/Error/bg-error.png'),
         // Tab navigation icons
         require('./navigation/icon-team-active.png'),
         require('./navigation/icon-team-inactive.png'),
         require('./navigation/icon-health-check-active.png'),
         require('./navigation/icon-health-check-inactive.png'),
         require('./navigation/icon-diagram-active.png'),
-        require('./navigation/icon-diagram-inactive.png')
+        require('./navigation/icon-diagram-inactive.png'),
+        // Error icon
+        require('./components/Error/error-icon.png')
     ]
 
 
