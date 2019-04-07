1e305792a9113ae3fa0915437812e2c9db5808f6
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-04-07 12:20:31 +0200

    Fixed keyboard avoiding view on android for add team

--- screens/Teams/AddTeam/AddTeam.screen.js
+++ screens/Teams/AddTeam/AddTeam.screen.js
@@ -6,26 +6,26 @@ import { addTeam } from '../../../adapters/api'
 import { Button, Header, Page, TeamLogo } from '../../../components'
 import { labelStyle, inputStyle } from '../../../constants/Style'
 
-const LogoWrapper = styled.View`
-  flex: 1;
-  align-items: center;
-`
-
-const Footer = styled.View`
-    flex: 3;
-    justify-content: flex-end;
-    margin-bottom: 30px;
-`
-
 const HeaderWrapper = styled.View`
   margin-bottom: 30px;
 `
 
-export default class AddTeamScreen extends React.Component {
-    static navigationOptions = {
-        header: null
-    }
+const PageContent = styled.View`
+  flex: 1;
+  justify-content: space-between;
+`
+const Top = styled.View`
+  justify-content: center;
+  align-items: center;
+`
+const Footer = styled.View``
+const Middle = styled.View`
+  flex: 1;
+  justify-content: center;
+  margin-top: 45px;
+`
 
+export default class AddTeamScreen extends React.Component {
     state = {
         name: ''
     }
@@ -57,23 +57,29 @@ export default class AddTeamScreen extends React.Component {
                             </TouchableOpacity>
                         }/>
                     </HeaderWrapper>
-                    <LogoWrapper>
-                        <TeamLogo name={this.state.name} size={100}/>
-                    </LogoWrapper>
-                    <Form style={{ justifyContent: 'space-around', marginRight: 15, marginTop: 30 }}>
-                        <Item floatingLabel>
-                            <Label style={labelStyle}>Team name</Label>
-                            <Input style={inputStyle}
-                                   autoCorrect={false}
-                                   autoCapitalize='words'
-                                   textContentType='givenName'
-                                   value={this.state.name}
-                                   onChangeText={(val) => this.onTextChange('name', val)}/>
-                        </Item>
-                    </Form>
-                    <Footer>
-                        <Button version='secondary' onPress={() => this.addTeam()} text='Add'/>
-                    </Footer>
+
+                    <PageContent>
+                        <Top>
+                            <TeamLogo name={this.state.name} size={100}/>
+                        </Top>
+                        <Middle>
+                            <Form style={{ justifyContent: 'center', marginRight: 15 }}>
+                                <Item floatingLabel>
+                                    <Label style={labelStyle}>Team name</Label>
+                                    <Input style={inputStyle}
+                                           autoCorrect={false}
+                                           autoCapitalize='words'
+                                           textContentType='givenName'
+                                           value={this.state.name}
+                                           onChangeText={(val) => this.onTextChange('name', val)}/>
+                                </Item>
+                            </Form>
+                        </Middle>
+                        <Footer>
+                            <Button version='secondary' onPress={() => this.addTeam()} text='Add'/>
+                        </Footer>
+                    </PageContent>
+
                 </KeyboardAvoidingView>
             </Page>
         )
