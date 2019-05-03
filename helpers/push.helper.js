cc15d2b834c8e92e4e36e80148b34ed685908f31
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-05-03 21:35:10 +0200

    Push notifications

--- helpers/push.helper.js
+++ helpers/push.helper.js
@@ -2,16 +2,16 @@ import { Permissions, Notifications } from 'expo'
 import {registerForPushNotifications} from '../adapters/api'
 
 export async function register() {
-    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS)
+    let { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS)
 
     if (status !== 'granted') {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS)
-
-        if (status !== 'granted') {
-            console.log('User did not grant a permission')
-        } else {
+        try{
+            await Permissions.askAsync(Permissions.NOTIFICATIONS)
             let token = await Notifications.getExpoPushTokenAsync()
             return registerForPushNotifications(token)
+        }
+        catch(e){
+            console.log({'e': e})
         }
     }
 }
